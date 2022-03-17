e342e2519c07873bf69310e0d4ec85a6580e3519
charanvir <charanvir123@gmail.com>
2022-03-17 02:46:57 -0400

    appended issues to second HTML, added display to indiciate if there are more than 30 issues and dynamically created a link to take user to Github to see more issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issue-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,7 +9,12 @@ var getRepoIssues = function (repo) {
             if (response.ok) {
                 response.json().then(function (data) {
                     displayIssues(data);
-                })
+
+                    // check if api has paginated issues
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    };
+                });
             }
             else {
                 alert("There was a problem with your request!")
@@ -51,4 +57,17 @@ var displayIssues = function (issues) {
     }
 }
 
-getRepoIssues("Charanvir/ScoreKeeper-Project")
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To See more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // apend to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react")
